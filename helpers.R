964ed17eb93a6e0228c9e2472cbe3d90a1fe858f
Unknown <nicolaskeller92@googlemail.com>
2020-04-04 15:47:06 +0200

    merged four tables to one table

---
+++ helpers.R
@@ -0,0 +1,33 @@
+library(data.table)
+
+join_data <- function(data_list) {
+  tmp_a <- data.table(data_list$altas)
+  tmp_c <- data.table(data_list$casos)
+  tmp_f <- data.table(data_list$falle)
+  tmp_h <- data.table(data_list$hospi)
+  tmp_a[, total_altas := total] 
+  tmp_c[, total_casos := total]
+  tmp_f[, total_falle := total]
+  tmp_h[, total_hospi := total]
+  tmp1 <- merge(tmp_c, tmp_a, by = c("fecha", "CCAA"), all.x=TRUE)
+  tmp2 <- merge(tmp1, tmp_f, by = c("fecha", "CCAA"), all.x=TRUE)
+  tmp3 <- merge(tmp2, tmp_h, by = c("fecha", "CCAA"), all.x=TRUE)
+  cols <- c("fecha", "CCAA", "total_altas", "total_falle", "total_casos", "total_hospi")
+  tmp3 <- tmp3[, ..cols]
+  tmp3[is.na(total_falle), total_falle := 0]
+  tmp3[is.na(total_altas), total_altas := 0]
+  tmp3[is.na(total_hospi), total_hospi := 0]
+  tmp3[, activos := total_casos - (total_falle + total_altas)]
+  return (tmp3)
+}
+
+# data_list <- list(
+#   "altas" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_altas_long.csv")),
+#   "casos" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_casos_long.csv")),
+#   "falle" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv")),
+#   "hospi" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv"))
+# )
+# 
+# my_dat <- join_data(data_list)
+# View(my_dat)
+
--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(data.table)
 library(magrittr)
 library(readr)
+source("helpers.R")
 
 shinyServer(function(input, output, session) {
   
@@ -10,22 +11,33 @@ shinyServer(function(input, output, session) {
     "falle" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv")),
     "hospi" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv"))
   )
+  my_data <- join_data(data_list)
   
-  get_data <- reactive({
-    if (input$choose_data == "Casos") {tmp <- data_list$casos} 
-    else if (input$choose_data == "Altas") {tmp <- data_list$altas}
-    else if (input$choose_data == "Hospitalizados") {tmp <- data_list$hospi}
-    else {tmp <- data_list$falle}
-    setDT(tmp)
-  })
+  
+  # get_data <- reactive({
+  #   if (input$choose_data == "Casos") {tmp <- data_list$casos} 
+  #   else if (input$choose_data == "Altas") {tmp <- data_list$altas}
+  #   else if (input$choose_data == "Hospitalizados") {tmp <- data_list$hospi}
+  #   else {tmp <- data_list$falle}
+  #   setDT(tmp)
+  # })
   
   output$mainplot <- renderPlotly({
-    tmp <- get_data()
+    tmp <- copy(my_data)
+    print(tmp)
     if (input$total_or_ccaa == "Total") {
       tmp <- tmp[CCAA == "Total", ]
     } else {
       tmp <- tmp[!CCAA == "Total", ]
     }
+    if (input$choose_data == "Fallecidos") {my_col <- "total_falle"}
+    else if (input$choose_data == "Casos") {my_col <- "total_casos"}
+    else if (input$choose_data == "Hospitalizados") {my_col <- "total_hospi"}
+    else if (input$choose_data == "Altas") {my_col <- "total_altas"}
+    else if (input$choose_data == "Activos" ) {my_col <- "activos"}
+    
+    tmp[, total := tmp[[my_col]]]
+    
     tmp[, lag_total := c(NA, total[-.N]), by=CCAA]
     tmp[, diff := total - lag_total] 
     tmp[, diff_rel := ((total / lag_total) * 100) - rep(100, nrow(tmp))]
