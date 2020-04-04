59d3916392c99f521b4dadb467d86823883c78d0
Unknown <nicolaskeller92@googlemail.com>
2020-04-04 13:19:12 +0200

    added local files

---
+++ server.R
@@ -0,0 +1,47 @@
+library(data.table)
+library(magrittr)
+
+shinyServer(function(input, output, session) {
+  
+  data_list <- list(
+    "altas" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_altas_long.csv")),
+    "casos" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_casos_long.csv")),
+    "falle" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv")),
+    "hospi" = read_csv(url("https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/ccaa_covid19_fallecidos_long.csv"))
+  )
+  
+  get_data <- reactive({
+    if (input$choose_data == "Casos") {tmp <- data_list$casos} 
+    else if (input$choose_data == "Altas") {tmp <- data_list$altas}
+    else if (input$choose_data == "Hospitalizados") {tmp <- data_list$hospi}
+    else {tmp <- data_list$falle}
+    setDT(tmp)
+  })
+  
+  output$mainplot <- renderPlotly({
+    tmp <- get_data()
+    if (input$total_or_ccaa == "Total") {
+      tmp <- tmp[CCAA == "Total", ]
+    } else {
+      tmp <- tmp[!CCAA == "Total", ]
+    }
+    tmp[, lag_total := c(NA, total[-.N]), by=CCAA]
+    tmp[, diff := total - lag_total] 
+    tmp[, diff_rel := ((total / lag_total) * 100) - rep(100, nrow(tmp))]
+    if (input$stat == "Cambio absoluto") {
+      show_col <- "diff"
+      yaxis <- "Cambio del número absoluto respecto al día anterior"
+    } else if (input$stat == "Cambio relativo") {
+      show_col <- "diff_rel"
+      yaxis <- "% cambio respecto al día anterior"
+    } else {
+      show_col <- "total"
+      yaxis <- "Número acumulado"
+    }
+    plot_ly(x = tmp$fecha, y = tmp[[show_col]], color = tmp$CCAA, type="bar") %>% 
+      layout(title = paste0(input$stat, " de ", input$choose_data, " por fecha"), 
+             xaxis = list(title = "Fecha", tickmode='linear'), 
+             yaxis = list(title = yaxis))
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(shinythemes)
+library(plotly)
+
+shinyUI(fluidPage(
+  includeCSS("www/styles.css"),
+  theme = shinytheme("cerulean"),
+  fluidRow(
+    column(
+      2, 
+      selectInput(
+        "choose_data", 
+        "Elige datos", 
+        choices = c("Casos", "Altas", "Hospitalizados", "Fallecidos")
+      ),
+      selectInput(
+        "total_or_ccaa",
+        "Total o por CCAA", 
+        choices = c("Total", "Por CCAA")
+      ),
+      selectInput(
+        "stat", 
+        "Elige estadística",
+        choices=c("Acumulado", "Cambio absoluto", "Cambio relativo")
+      )
+    ),
+    column(
+      10, 
+      plotlyOutput(
+        "mainplot",
+        height=800
+      )
+    )
+  )
+))
